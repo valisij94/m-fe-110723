64a355cb5c4b1d2e8676623f15b04dff5d54b41b
Davy <davyd.dabahian@cheetahdigital.com>
2023-11-01 22:39:23 +0200

    материалы 11-го занятия - работа с DOM

---
+++ practice_11_array_DOM/scripts/main.js
@@ -0,0 +1,61 @@
+// Подопытные данные
+
+// Массив чисел (примитивы)
+const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11,9,2,7];
+
+// Массив строк (примитивы)
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const products = [
+    {
+        name: "Ball",
+        category: "Sport",
+        img: "./assets/ball.jpeg",
+        price: 100,
+        description: 'Just the simple ball'
+    },
+    {
+        name: "Gloves",
+        category: "Sport",
+        img: "./assets/gloves.jpg",
+        price: 140,
+        description: 'Tactical gloves'
+    },
+    {
+        name: "Sport shoes",
+        category: "Sport",
+        discount: true,
+        img: "./assets/shoes.png",
+        price: 320,
+        description: 'Sport shoes. The best choice for running'
+    },
+    {
+        name: "Hammer",
+        category: "Tools",
+        img: "./assets/hammer.jpeg",
+        price: 40,
+        description: 'The best way to convince somebody that you are right.'
+    },
+    {
+        name: "Saw",
+        category: "Tools",
+        discount: true,
+        img: "./assets/saw.jpeg",
+        price: 75,
+        description: 'This will help you in case the hammer could not convince your companion'
+    },
+    {
+        name: "Shark toy",
+        category: "Other",
+        img: "./assets/shark.jpeg",
+        price: 45,
+        description: 'From IKEA with love'
+    },
+    {
+        name: "Truck",
+        category: "Other",
+        img: "./assets/truck.jpeg",
+        price: 80,
+        description: 'Truck. Nothing more.'
+    }
+];
