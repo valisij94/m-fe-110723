9edd79ad75a5bbce5d92bac38214813f69a0fefe
Davy <davyd.dabahian@cheetahdigital.com>
2023-11-02 15:31:38 +0200

    результаты 11го занятия

--- practice_11_array_DOM/scripts/main.js
+++ practice_11_array_DOM/scripts/main.js
@@ -59,3 +59,76 @@ const products = [
         description: 'Truck. Nothing more.'
     }
 ];
+
+
+/* 1. Вспоминаем работу с методами массивов. У нас есть массив объектов `products` Нужно получить из него массив названий товаров. Решить задачу двумя способами: через `map`, и через `reduce`.
+*/
+
+const productNames = products.map( (product) => product.name );
+//console.log('From map', productNames);
+
+const productNamesFromReduce = products.reduce(
+    (accum, product) => {
+        accum.push(product.name);
+        return accum;
+    },
+    []
+);
+//console.log('From reduce', productNamesFromReduce)
+
+/* 2. Из массива строк, получить объект, в котором ключами будут сами строки, а значениями - длины этих строк. */
+
+const myObj = {
+    prop1: 'value1',
+    prop2: 'value2'
+}
+
+const val = myObj.prop1;
+let myVar = 'prop2';
+
+const val2 = myObj[ myVar ]; // myObj.prop2
+
+
+const stringsObject = strings.reduce(
+    ( accum, str ) => {
+        accum[str] = str.length;
+        return accum;
+    },
+    {}
+);
+console.log(stringsObject);
+
+/* 3. Из массива чисел получить объект, который будет содержать 2 свойства: `even`, `odd`. Оба свойства - массивы. В первом массиве должны быть нечетные числа из исходного массива, а во втором - четные. */
+
+const evenOddNumbers = numbers.reduce(
+    (accum, value) => {
+        if (value % 2 === 0) {
+            accum.even.push(value);
+        }
+        else {
+            accum.odd.push(value);
+        }
+        return accum;
+    },
+    {
+        even: [],
+        odd: []
+    }
+)
+
+console.log(evenOddNumbers)
+
+/* 4. Из массива объектов `products`, получить массив, который содержит уникальные категории товаров. НЕ использовать Set!
+*/
+const uniqueCategories = products.reduce(
+    (accum, product) => {
+        const cat = product.category;
+        if (!accum.includes(cat)) {
+            accum.push(cat);
+        }
+        return accum;
+    },
+    []
+)
+
+console.log(uniqueCategories);
