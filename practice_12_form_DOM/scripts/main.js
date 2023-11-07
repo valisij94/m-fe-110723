6a14eb5440d8b35cee132d6f77dec5e7a54d72c8
Davy <davyd.dabahian@cheetahdigital.com>
2023-11-07 10:46:46 +0200

    материалы 12-го занятия - DOM, HTMLForm

---
+++ practice_12_form_DOM/scripts/main.js
@@ -0,0 +1,54 @@
+// Подопытные данные
+const products = [
+    {
+        name: "Ball",
+        category: "Sport",
+        img: "./assets/ball.jpeg",
+        price: 100,
+        description: 'Just the simple ball'
+    },
+    {
+        name: "Gloves",
+        category: "Sport",
+        img: "./assets/gloves.jpg",
+        price: 140,
+        description: 'Tactical gloves'
+    },
+    {
+        name: "Sport shoes",
+        category: "Sport",
+        discount: true,
+        img: "./assets/shoes.png",
+        price: 320,
+        description: 'Sport shoes. The best choice for running'
+    },
+    {
+        name: "Hammer",
+        category: "Tools",
+        img: "./assets/hammer.jpeg",
+        price: 40,
+        description: 'The best way to convince somebody that you are right.'
+    },
+    {
+        name: "Saw",
+        category: "Tools",
+        discount: true,
+        img: "./assets/saw.jpeg",
+        price: 75,
+        description: 'This will help you in case the hammer could not convince your companion'
+    },
+    {
+        name: "Shark toy",
+        category: "Other",
+        img: "./assets/shark.jpeg",
+        price: 45,
+        description: 'From IKEA with love'
+    },
+    {
+        name: "Truck",
+        category: "Other",
+        img: "./assets/truck.jpeg",
+        price: 80,
+        description: 'Truck. Nothing more.'
+    }
+];
