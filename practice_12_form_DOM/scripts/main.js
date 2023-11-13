530194d8e459d03c6f14650c0b4faba7e630173c
Davy <davyd.dabahian@cheetahdigital.com>
2023-11-13 18:25:07 +0200

    материалы 13-го занятия - localStorage

--- practice_12_form_DOM/scripts/main.js
+++ practice_12_form_DOM/scripts/main.js
@@ -96,3 +96,65 @@ cancelBtn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log('The Cancel button is pressed 2');
 });
+
+/*
+4. Работаем с карточками товаров. Массив `products` содержит данные о товарах. Нужно научиться рендерить карточки товаров. Первая задача - делаем функцию отрисовки карточки товара `renderProduct`. Она принимает в аргументе объект с данными товара.
+ - сама карточка - это `div` (контейнер), он должен иметь класс `productCard`
+ - карточка содержит название товара, картинку товара, цену, категорию и описание. Название - `h3` с классом `productTitle`, цена - параграф с классом `productPrice`, категория - параграф с классом `productCategory`, картинка - изображение с классом `productImg`, и описание - параграф с классом `productDescription`. Изображению нужно проставить атрибут `src`, если у объекта с данными товара существует свойство `img`.
+ - функция должна создать и вернуть DOM-узел, но НЕ встраивать его в DOM.
+ */
+
+const productsContainer = document.querySelector('.productsContainer');
+
+function renderProduct(product){
+    const productCard = document.createElement('div');
+    productCard.classList.add('productCard');
+
+    productCard.addEventListener('click', (event) => {
+        productCard.classList.toggle('productCardHighlighted');
+    });
+
+    const productTitle = document.createElement('h3');
+    productTitle.textContent = product.name;
+    productTitle.classList.add('productTitle');
+
+    const productPrice = document.createElement('p');
+    productPrice.textContent = product.price;
+    productPrice.classList.add('productPrice');
+
+    const productCategory = document.createElement('p');
+    productCategory.textContent = product.category;
+    productCategory.classList.add('productCategory');
+
+    const productImg = document.createElement('img');
+    productImg.src = !!product.img ? product.img : '';
+    productImg.classList.add('productImg');
+
+    const productDescription = document.createElement('p');
+    productDescription.textContent = product.description;
+    productDescription.classList.add('productDescription');
+
+    productCard.append(productImg, productTitle, productPrice, productCategory, productDescription);
+
+    return productCard;
+}
+
+const prod = renderProduct(products[0]);
+productsContainer.append(prod);
+/* 5. Теперь напишем функцию отрисовки списка товаров `renderProductList`. Она должна принять массив товаров, и отрисовать эти товары в `div.productContainer`. То есть, здесь нам придется вспомнить методы массивов. Нужно пройтись по массиву, и для каждого элемента создать DOM-элемент (карточку товара) и добавить в контейнер `div.productContainer`
+*/
+
+function renderProductList(products) {
+    productsContainer.innerHTML = '';
+    products.forEach( product => {
+        const renderedProduct = renderProduct(product);
+        productsContainer.append(renderedProduct);
+    });
+}
+
+renderProductList(products)
+// FALSY-values: null, undefined, '', 0, NaN, false
+
+/*
+6. Добавляем немного интерактива. Нужно чтобы при клике на карточку, к ней применялся класс `productCardHighlighted`. А при повторном клике - этот класс должен быть удален с карточки.
+*/
