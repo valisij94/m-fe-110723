030fdaa1b527f04b98a21de2e1bf69ba8645b311
Davy <davyd.dabahian@cheetahdigital.com>
2024-01-08 23:17:41 +0200

    материалы 25-го занятия

--- practice_24_OOP_static_getters_setters/scripts/main.js
+++ practice_24_OOP_static_getters_setters/scripts/main.js
@@ -1,9 +1,52 @@
 class Product {
 
+  #purchasePrice;
+
+  static purchaseCoefficient = 0.75;
+  static #instancesCounter = 0;
+
   constructor(title, category, price, description) {
     this.title = title;
     this.category = category;
     this.price = price;
     this.description = description;
+    this.#purchasePrice = Product.purchaseCoefficient * price;
+    Product.#instancesCounter = Product.#instancesCounter + 1;
+  }
+
+  getAnnotation() {
+    return `Product ${this.title}, price - ${this.price}`;
+  }
+
+  get annotation() {
+    return `Product ${this.title}, price - ${this.price}`;
+  }
+
+  get purchasePrice() {
+    return this.#purchasePrice;
+  }
+
+  set purchasePrice(value) {
+    if (value > this.price) {
+      throw new Error('Purchase price should be less than price!')
+    }
+    this.#purchasePrice = value;
+  }
+
+  static setPurchaseCoefficient(value) {
+    if (value > 0 && value < 1) {
+      Product.purchaseCoefficient = value;
+    }
+  }
+
+  static get instancesCounter() {
+    return Product.#instancesCounter;
   }
 }
+
+const p = new Product('Ball', 'Sport', 100, 'Super Ball!');
+console.log(Product.instancesCounter);
+const p1 = new Product('Ball', 'Sport', 100, 'Super Ball!');
+console.log(Product.instancesCounter)
+const p2 = new Product('Ball', 'Sport', 100, 'Super Ball!');
+console.dir(Product)
---
+++ practice_25_prepare_react/scripts/main.js
@@ -0,0 +1,35 @@
+const dwarf = {
+  name: 'Torin',
+  surname: 'Oakshield',
+  race: 'Dwarwes'
+};
+
+const armedDwarf = {
+  name: 'Dain',
+  surname: 'Ironfoot',
+  race: 'Dwarwes',
+  weapons: ['hammer', 'axe', 'knife']
+};
+
+const dwarfWarrior = {
+  name: 'Dvalin',
+  surname: '',
+  race: 'Dwarwes',
+  weapons: ['axe', 'axe', 'hammer', 'knife'],
+  armours: {
+    helmet: null,
+    armour: 'Leather armour',
+    shield: 'Iron shield'
+  }
+};
+
+const dwarfSage = {
+  name: 'Balin',
+  surname: '',
+  race: 'Dwarwes',
+  weapons: ['hammer'],
+  skills: {
+    magic: null,
+    archery: 3
+  }
+}
