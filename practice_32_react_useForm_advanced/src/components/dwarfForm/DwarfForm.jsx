08ca42a90e1d3f222b660b680c3a3ecd8d16ca31
Davy <davyd.dabahian@cheetahdigital.com>
2024-02-01 15:30:20 +0200

    practice 32 results

--- practice_32_react_useForm_advanced/src/components/dwarfForm/DwarfForm.jsx
+++ practice_32_react_useForm_advanced/src/components/dwarfForm/DwarfForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 import classes from './DwarfForm.module.css';
 
@@ -9,11 +9,46 @@ export default function DwarfForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
+    getValues
   } = useForm();
 
-  const dwarfSubmitHandler = (formData) => {
-    console.log(formData);
+  const [formError, setFormError] = useState('');
+
+  const checkForm = () => {
+    const formInputs = getValues();
+    let errorMessage = '';
+    for (let [key, value] of Object.entries(formInputs)) {
+      if (!value) {
+        errorMessage += `Field ${key} is empty!`;
+      }
+    }
+    setFormError(errorMessage);
+  }
+
+  const dwarfSubmitHandler = async (formData) => {
+    if (formData.dwarfName === 'Dvalin' && formData.dwarfWeapon === 'Sword') {
+      setFormError('You already have 2 axes!');
+    }
+    else {
+      setFormError('');
+
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+          headers: {'Content-type': 'application/json; charset=UTF-8'},
+          method: 'POST',
+          body: JSON.stringify(formData)
+        });
+        if (!response.ok) {
+          throw new Error('Failed to submit!');
+        }
+        const result = await response.json();
+        console.log(result);
+      }
+      catch (error) {
+        setFormError(error.message);
+      }
+    }
   }
 
   const dwarfErrorHandler = (formErrors) => {
@@ -31,7 +66,15 @@ export default function DwarfForm() {
         className={classes.dwarfFormInput}
         type="text"
         {...register('dwarfName', {
-          required: 'Please, enter your name!'
+          required: 'Please, enter your name!',
+          minLength: {
+            value: 3,
+            message: 'Too short!'
+          },
+          maxLength: {
+            value: 12,
+            message: 'Too long!'
+          }
         })}
       />
       {
@@ -42,14 +85,40 @@ export default function DwarfForm() {
         className={classes.dwarfFormInput}
         type="text"
         {...register('dwarfWeapon', {
-          required: 'Choose your weapon!'
+          required: 'Choose your weapon!',
+          validate: (value) => value.toLowerCase() !== 'crossbow' || 'No archers!'
         })}
       />
       {
         errors.dwarfWeapon && errors.dwarfWeapon.message && <p>{errors.dwarfWeapon.message}</p>
       }
-      <label htmlFor="name">Name</label>
-      <button className={classes.dwarfFormButton}>Join Torin's troop!</button>
+
+      <label htmlFor="email">Please enter email</label>
+      <input
+        className={classes.dwarfFormInput}
+        type="email"
+        {...register('dwarfEmail', {
+          required: 'Please enter your email!',
+          pattern: {
+            value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            message: 'Invalid email!'
+          }
+        })}
+      />
+      {
+        errors.dwarfEmail && errors.dwarfEmail.message && <p>{errors.dwarfEmail.message}</p>
+      }
+
+      { formError && <p>{formError}</p>}
+      <button
+        type="button"
+        className={classes.dwarfFormButton}
+        onClick={checkForm}
+      >Check your data!</button>
+      <button
+        type="submit"
+        className={classes.dwarfFormButton}
+      >Join Torin's troop!</button>
     </form>
 
   );
