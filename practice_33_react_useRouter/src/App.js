d7b114de86d5999c9b5de999256259be4720fa55
Davy <davyd.dabahian@cheetahdigital.com>
2024-02-05 21:49:05 +0200

    practice_33 react-router

---
+++ practice_33_react_useRouter/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import DwarfForm from './components/dwarfForm/DwarfForm';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h2>Welcome, Stranger! Torin the OakShield invites you to join his voyage to Lonely Mountain! Please, fill this form ASAP!</h2>
+        <DwarfForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
