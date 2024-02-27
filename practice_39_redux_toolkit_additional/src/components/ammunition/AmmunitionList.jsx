d7386a96e5c0a58fb0350e300c596fe4f02b9589
Davy <davyd.dabahian@cheetahdigital.com>
2024-02-27 13:24:26 +0200

    add some test data

--- practice_39_redux_toolkit_additional/src/components/ammunition/AmmunitionList.jsx
+++ practice_39_redux_toolkit_additional/src/components/ammunition/AmmunitionList.jsx
@@ -8,43 +8,50 @@ const ammos = [
     id: 1,
     name: 'Axe',
     description: 'Double axe. With him you are not afraid of any enemy!',
-    weight: 9.5
+    weight: 9.5,
+    category: 'melee'
   },
   {
     id: 2,
     name: 'Mitril Armour',
     description: 'A masterpiece of blacksmithing from Moria. Will protect you from an ax, arrow, and anything else',
-    weight: 1.2
+    weight: 1.2,
+    category: 'armour'
   },
   {
     id: 3,
     name: 'WarHammer',
     description: 'Not a game, not a tool - just a WAR HAMMER!',
-    weight: 11.3
+    weight: 11.3,
+    category: 'melee'
   },
   {
     id: 4,
     name: 'Orc Crossbow',
     description: 'Hard crossbow to deal with all types of armours',
-    weight: 5.4
+    weight: 5.4,
+    category: 'archery'
   },
   {
     id: 5,
     name: 'Elves Bow',
     description: 'Not the best choice if you are not an elve.. But you can try!',
-    weight: 3.1
+    weight: 3.1,
+    category: 'archery'
   },
   {
     id: 6,
     name: 'Spear',
     description: 'Nice choice to deal with enemies on the horses',
-    weight: 7
+    weight: 7,
+    category: 'melee'
   },
   {
     id: 7,
     name: 'Iron Shield',
     description: 'Should save your liver (bot it does not ensured...)',
-    weight: 6.4
+    weight: 6.4,
+    category: 'armour'
   }
 ];
 
